016e83a52a59786f01a2336aa217e0c179432b33
krishna <krishnavamshi.k143@gmail.com>
2025-11-22 06:37:23 +0000

    added a even number logic

--- user.sh
+++ user.sh
@@ -2,4 +2,10 @@
 #this script is all about to find even numbers
 read -p "please enter the number:" NUM
 echo "entered number is :" $NUM
+if [  expr $NUM % 2 = 0 ]
+then
+	echo"it is an even number"
+else
+	echo "is an odd"
+fi
 
